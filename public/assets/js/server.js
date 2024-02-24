532d65644cfd870b7c324bd9f5814e496977fe2b
emily <emilycassel77@gmail.com>
2024-02-24 16:00:04 -0500

    app.post pathway .get pathway

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -7,9 +7,38 @@ const Port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/api/notes", (req, res) => {});
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "db.json"), "utf8", (err, data) => {
+        if (err) {
+            return res.status(500).json({error: "Computer Error"})
+        }
+    })
+});
 
-app.port("/api/notes", (req, res) => {}); 
+app.port("/api/notes", (req, res) => {
+    const {title, text} = req.body
+    if(!title || !test) {
+        return res.status(404).json({error: "Must have text and title"})
+    } 
+    fs.readFile(path.join(__dirname, "db,json"), "utf8", (err, data) => {
+        if (err){
+            return res.status(404).json({error: "Computer error"})
+        }
+    const notes = JSON.parse(data); 
+    const newNote = {
+            id: 
+            title, 
+            text, 
+        }
+    notes.push(newNote); 
+    fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(notes), (err) => {
+        if(err) {
+            return res.status(404).json({error: "Computer error"})
+        }
+        res.status().json(newNote);
+    })
+    })
+}); 
 
 app.delete("/api/notes/:id", (req, res) => {}); 
 
